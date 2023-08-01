85af16ababceaa86ab3cf97a1d8010396f9da1e3
dusixx <113414935+dusixx@users.noreply.github.com>
2023-08-01 23:30:34 +0300

    small fix

--- 1/x.js
+++
@@ -1,8 +0,0 @@
-import path from 'path';
-console.log(path.dirname(process.argv[1]));
-
-const found = movies.find(...);
-if (!found) return null;
-
-updateMovies(movies.filter(...));
-return found;
